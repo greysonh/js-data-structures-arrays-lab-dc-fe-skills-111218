264a44dcf32c753a2d07e844b2ab80b6978b3cb3
Greyson Harris <gharris@esri.com>
2018-12-17 23:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver() {
   drivers.push ("Ralph");
 }
+
+function descructivelyPrependDriver() {
+  drivers.shift ("Bob");
+}
