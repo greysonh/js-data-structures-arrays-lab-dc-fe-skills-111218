d7fb17d77f4ef3f1ac6a743d35ea0921082dac4d
Greyson Harris <gharris@esri.com>
2018-12-18 00:34:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+ return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+ return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  return drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+ return drivers.shift();
+}
+
+function appendDriver(name) {
+return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  list = [...drivers];
+  list.pop();
+  return list;
+}
+
+function removeFirstDriver() {
+  list = [...drivers];
+  list.shift();
+  return list;
+}
